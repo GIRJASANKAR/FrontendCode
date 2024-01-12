921172086788a0d5f28ab0635c971720215dc091
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2024-01-12 11:14:45 +0530

    update

--- src/customHooks/useTimeout.tsx
+++ src/customHooks/useTimeout.tsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useRef } from "react";
 
+// Create a hook to easily use setTimeout(callback, delay).
+
+// 1- reset the timer if delay changes
+// 2- DO NOT reset the timer if only callback changes
+
+
 export function useTimeout(callback: () => void, delay: number) {
   const callbackref=useRef(callback);
   callbackref.current=callback;
