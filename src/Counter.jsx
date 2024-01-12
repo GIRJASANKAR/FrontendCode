67260336c1d84419e3574f0fc9f2f6cacd339981
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2024-01-12 11:08:31 +0530

    counter

---
+++ src/Counter.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+
+  const updateCount = () => {
+    setCount(prevCount => prevCount + diff)
+  }
+
+  return (
+    <div>
+      <button data-testid="decrement-button" onClick={()=>updateCount(-1)}>-</button>
+      <button data-testid="increment-button" onClick={()=>updateCount(1)}>+</button>
+      <p>clicked: {count}</p>
+    </div>
+  )
+}
