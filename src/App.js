8a178dbaec59e27f6c8e331c7734e0ade920420c
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2024-01-29 14:59:22 +0530

    .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App text-4xl flex justify-center items-center h-screen">
      <Accordion/>
-     nbkbkjhjb,jkbkjblkjhoj.ibghjhgjhv
     </div>
   );
 }
